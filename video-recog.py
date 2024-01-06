c3af0660379362f0724002d42b8f83551b29f52e
nathan <nathankdgray@gmail.com>
2024-01-06 10:05:58 -0800

    intro to build
    - imported cv2 for processing
    - using deepface for the facial analysis

--- video-recog.py
+++ video-recog.py
@@ -3,5 +3,7 @@
 import cv2		# used for image/video processing
 from deepface import DeepFace		# used for facial analysis "deepface"
 
+# uses pre-trained emotion recognition library from deepface
+model = DeepFace.build_model("Emotion")
 
 
